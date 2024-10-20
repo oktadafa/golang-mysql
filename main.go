51876c4a51f3d135fc8811cc2e40e1594356214f
oktadafa <oktadafasampang@gmail.com>
2024-10-20 19:22:31 +0700

    add fitur admin

--- main.go
+++ main.go
@@ -4,86 +4,141 @@ import (
 	"fmt"
 )
 
+// Definisikan variabel global untuk menyimpan daftar user
+var listUser = []User{
+	{
+		username: "Admin",
+		password: "Admin",
+		status:   1,
+	},
+	{
+		username: "client",
+		password: "client",
+		status:   2,
+	},
+}
+
+var listMenu = []Food{
+	{
+		name:  "nasi goreng",
+		price: 15.000,
+		tipe:  FoodType,
+	},
+	{
+		name:  "orange juice",
+		price: 5.000,
+		tipe:  DrinkType,
+	},
+	{
+		name:  "lele goreng",
+		price: 13.000,
+		tipe:  FoodType,
+	},
+}
+
 type User struct {
 	username string
 	password string
 	status   int
 }
 
+type TypeMenu string
+
+const (
+	FoodType  TypeMenu = "food"
+	DrinkType TypeMenu = "drink"
+)
+
+type Food struct {
+	name  string
+	price float64
+	tipe  TypeMenu
+}
+
 func main() {
-	var listUser = []User{
-		{
-			username: "Admin",
-			password: "Admin",
-			status:   1,
-		},
-		{
-			username: "client",
-			password: "client",
-			status:   2,
-		},
-	}
+	menu() // Memanggil menu utama
+}
+
+func menu() {
 	var nomor int
 	var user User
-	fmt.Println("Silahkan Pilih Nomor Dari Pilihan Beriku :\n" +
-		"1.Login \n" +
-		"2.Register")
-	fmt.Print("Masukan Nomor :")
+	fmt.Println("Silahkan Pilih Nomor Dari Pilihan Berikut :\n" +
+		"1. Login \n" +
+		"2. Register")
+	fmt.Print("Masukkan Nomor :")
 	fmt.Scanln(&nomor)
+
 	switch nomor {
 	case 1:
-		user.login(listUser)
+		user.login(&listUser) // Panggil login
 		break
 	case 2:
-		register(&listUser)
+		register(&listUser) // Panggil register
 		break
 	default:
-		fmt.Println("Anda Memasukan Nomor Yang salah")
+		fmt.Println("Anda Memasukkan Nomor yang Salah")
 		break
 	}
 }
 
-func (user User) login(users []User) {
-	fmt.Print("Masukan Username = ")
+func (user User) login(users *[]User) {
+	fmt.Print("Masukkan Username = ")
 	fmt.Scanln(&user.username)
-	fmt.Print("Masukan Password = ")
+	fmt.Print("Masukkan Password = ")
 	fmt.Scanln(&user.password)
+
 	switch user.checkUser(users) {
 	case 1:
-		fmt.Println("Ini Adalah Admin")
+		Admin()
 		break
 	case 2:
 		fmt.Println("Ini Adalah Client")
 		break
 	default:
-		fmt.Println("Username Atau Password Yang Masukan Salah")
+		fmt.Println("Username atau Password yang Anda Masukkan Salah")
 		break
 	}
 }
 
-func (user User) checkUser(users []User) int {
-
-	for _, item := range users {
+func (user User) checkUser(users *[]User) int {
+	for _, item := range *users {
 		if user.username == item.username && user.password == item.password {
 			return item.status
 		}
 	}
 	return 0
 }
+
 func register(users *[]User) {
-	Adduser(users)
+	Adduser(users) // Tambahkan user baru
+	menu()         // Kembali ke menu setelah register
 }
+
 func Adduser(users *[]User) {
 	var user User
-	fmt.Print("masukan username : ")
+	fmt.Print("Masukkan Username: ")
 	fmt.Scanln(&user.username)
-	fmt.Print("Masukan Password : ")
+	fmt.Print("Masukkan Password: ")
 	fmt.Scanln(&user.password)
-	fmt.Println("Silahkan Pilih Role : \n" +
-		"1.Admin\n" + "2.Client\n masukan nomor role = ")
+	fmt.Println("Silahkan Pilih Role: \n1. Admin\n2. Client\nMasukkan nomor role:")
 	fmt.Scanln(&user.status)
 
-	*users = append(*users, user)
+	*users = append(*users, user) // Tambahkan user baru ke slice
 
-	fmt.Println(*users)
+	fmt.Println("User Baru Terdaftar:", *users)
+}
+
+func Admin() {
+	var nomor int
+	fmt.Println(
+		"___________________________________________________\n" +
+			"|-----------------Restoran Apa Saja----------------| \n" +
+			"|--------------------------------------------------|\n" +
+			"| 1.List Menu______________________________________|\n" +
+			"| 2.Add Menu_______________________________________|\n" +
+			"| 3.List User______________________________________|\n" +
+			"| 4.List Discount__________________________________|\n" +
+			"|__________________________________________________|\n" +
+			"Silahkan Pilih Nomor = ")
+	fmt.Scanln(&nomor)
 }
